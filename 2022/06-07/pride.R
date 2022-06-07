0fb42071597a2a0d324e91b00b367522e7d61b89
francescoolivo <francesco.olivo99@gmail.com>
2022-06-07 14:39:02 +0200

    2022-06-07 tidytuesday

--- 2022/06-07/pride.R
+++ 2022/06-07/pride.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(forcats)
 library(ggstream)
 library(scales)
-# library(prismatic)
+library(prismatic)
 library(ggtext)
 
 tuesdata <- tidytuesdayR::tt_load('2022-06-07')
@@ -26,10 +26,6 @@ ranking <-
                        labels = c("Others", "Toyota", "State Farm", "Southern Company", "FedEx", "Comcast", "AT&T", "Amazon"))) %>%
     arrange(rank)
 
-
-
-
-
 df <-
   contribution_data_all_states %>%
   filter(`Pride?` == TRUE, !is.na(Date)) %>%
@@ -40,12 +36,9 @@ df <-
   group_by(comp, year, total) %>%
   summarize(sum = sum(Amount)) %>%
   mutate(perc = sum / total)
-  
-df %>%
-  arrange(year)
 
 title = "Are they 
-    <span style='color:#FF0000;'>r</span><span style='color:#FFDB00;'>a</span><span style='color:#49FF00;'>i</span><span style='color:#00FF92;'>n</span><span style='color:#0092FF;'>b</span><span style='color:#4900FF;'>o</span><span style='color:#4900FF;'>w</span>-<span style='color:#FF0000;'>w</span><span style='color:#FFDB00;'>a</span><span style='color:#49FF00;'>s</span><span style='color:#00FF92;'>h</span><span style='color:#0092FF;'>i</span><span style='color:#4900FF;'>n</span><span style='color:#4900FF;'>g</span> you? "
+    <span style='color:#FF0000;'>r</span><span style='color:#FFDB00;'>a</span><span style='color:#49FF00;'>i</span><span style='color:#00FF92;'>n</span><span style='color:#0092FF;'>b</span><span style='color:#4900FF;'>o</span><span style='color:#FF00DB;'>w</span>-<span style='color:#FF0000;'>w</span><span style='color:#FFDB00;'>a</span><span style='color:#49FF00;'>s</span><span style='color:#00FF92;'>h</span><span style='color:#0092FF;'>i</span><span style='color:#4900FF;'>n</span><span style='color:#FF00DB;'>g</span> you? "
 
 df %>%
   ggplot(aes(x = year, y = sum, fill = fct_rev(comp))) +
@@ -55,7 +48,6 @@ df %>%
   scale_fill_manual(values = c(rainbow(7), "black")) +
   scale_x_continuous(breaks = seq(2013, 2022, 1), limits = c(2013, 2022)) +
   scale_y_continuous(breaks = seq(0, 1200000, 200000), labels = c("0k", "20k", "40k", "60k", "80k", "100k", "120k")) +
-  theme_minimal(base_size=9, base_family="Consolas") +
   labs(
     title = title,
     subtitle = "In the last five years, donations from companies that sponsor both prides\nand anti-LGBTQ politicians have grown drastically.",
@@ -63,6 +55,7 @@ df %>%
     x = "",
     y = ""
   ) +
+  theme_minimal(base_size=9, base_family="Consolas") +
   theme(
     panel.grid.minor = element_blank(),
     plot.background = element_rect(fill = 'ghostwhite', color = "ghostwhite"),
@@ -72,7 +65,6 @@ df %>%
     plot.caption = element_text(lineheight = 1.2, margin = margin(10, 0, 0, 0), hjust = 1),
     plot.margin = margin(10, 15, 10, 10),
     panel.grid.major.x = element_blank(),
-    # axis.title  = element_blank(),
     axis.text = element_text(color = "gray10"),
     legend.position = "top",
     legend.title = element_blank(),
@@ -80,9 +72,5 @@ df %>%
   ) +
   guides(fill=guide_legend(nrow = 2, byrow = FALSE))
 
-ggsave("2022/06-07/rainbow-washing.png", w = 6, h = 6, dpi = 600)
-
-
-
-
+ggsave("2022/06-07/rainbow-washing.png", w = 6, h = 6, dpi = 300)
 
